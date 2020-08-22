3a37c918e4d8d5e94dcb094a4abc0d5c4b009672
Akash Kundu <akashkundu92@gmail.com>
2020-08-21 20:35:55 -0700

    Initial Commit

---
+++ SwiftLintDiscovery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLintDiscovery
+//
+//  Created by Akash Kundu on 8/21/20.
+//  Copyright © 2020 akashkundu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
